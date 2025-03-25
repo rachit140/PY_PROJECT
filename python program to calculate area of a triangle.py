7fa6c397bfe82962265765fa193169ad897dc4ee
Rachit Sharma <86347608+rachit140@users.noreply.github.com>
2025-03-25 21:11:29 +0530

    Calculation of Area of Triangle
    Shows mathematical computation and logic.

---
+++ python program to calculate area of a triangle.py
@@ -0,0 +1,6 @@
+#python program to calculate area of a triangle
+#By Rachit Sharma on 14.01.2023
+Base = int(input("enter the Base of your tringle "))
+Height = int(input("enter the Height of your Triangle"))
+Area = 1/2*Base*Height
+print (Area)
