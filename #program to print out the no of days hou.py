5691de8b1756dbb8c760981c4f27f69717806ed3
Rachit Sharma <86347608+rachit140@users.noreply.github.com>
2025-03-25 21:14:17 +0530

    Add files via upload

---
+++ #program to print out the no of days hou.py
@@ -0,0 +1,12 @@
+#program to print out the no of days hours min. and seconds in that number of years
+#by rachit sharma 15.01.2023
+
+year = int(input("Enter Number of Years"))
+days = (year*365)
+hours = (year*8766)
+minutes = (year*525600)
+seconds = (year*31557600)
+print ("NO OF DAYS IN YEAR:",days)
+print ("NO OF HOURS IN YEAR:",hours)
+print ("NO OF MINUTES IN YEAR:",minutes)
+print ("NO OF SECONDS IN YEAR:",seconds)
