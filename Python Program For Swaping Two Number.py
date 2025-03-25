dfef77a963a3657f877c8a7818cf260e34fd1b7f
Rachit Sharma <86347608+rachit140@users.noreply.github.com>
2025-03-25 21:10:22 +0530

    Add files via upload

---
+++ Python Program For Swaping Two Number.py
@@ -0,0 +1,21 @@
+#defining the boiling and freezing temp in celcius
+boil = int(input("write down your boil value"))
+
+freezing = int(input("write down your freezing value"))
+
+
+print('Water Boiling temperature in Fahrenheit::')
+
+#Calculating Boiling temperature in Fahrenheit
+
+tb = boil * (9/5) + 32
+
+#Printing the temperature
+
+print(tb)
+
+print('Water Freezing temperature in Fahrenheit::')
+#Calculating Boiling temperature in Fahrenheit
+tf = freezing * (9/5) + 32
+#Printing the temperature
+print(tf)
