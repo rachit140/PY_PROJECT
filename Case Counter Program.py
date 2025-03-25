4ce4c8933e2b50ade1d84a0da88e71a3651b1d58
Rachit Sharma <86347608+rachit140@users.noreply.github.com>
2025-03-25 21:03:39 +0530

    Case Counter Program

---
+++ Case Counter Program.py
@@ -0,0 +1,20 @@
+data = input()
+u=l=d=a=0
+for i in range (len(data)):
+    if (data[i].isupper()):
+        u =u+1
+    if (data [i].islower()):
+        l = l+1
+    if (data[i].isalpha()):
+        a=a+1
+    if (data[i].isdigit()):
+        d=d+1
+print ("number of uppercase letter ",u)  
+print ("number of lowercase letter ",l)
+print ("number of alphabets letter ",a)
+print ("number of digits letters ",d)
+
+
+
+
+
